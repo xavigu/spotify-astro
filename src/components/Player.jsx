33c5b8fba5f4503e4edd95de0b6c4be2e06c3268
xavigu <xavigu91@gmail.com>
2023-10-12 19:26:19 +0200

    Persist player component state between pages

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const Pause = ({ className }) => (
   <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
@@ -18,6 +18,23 @@ export const Volume = () => (
 
 export function Player() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(null);
+  const audioRef = useRef();
+
+  useEffect(() => {
+    audioRef.current.src = `/music/1/01.mp3`;
+  }, []);
+
+  const handleClick = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.volume = 0.6;
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  }
+
   return (
     <div className="flex flex-row justify-between w-full px-4 z-50">
       <div className="">
@@ -26,7 +43,7 @@ export function Player() {
 
       <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center">
-          <button className="bg-white p-2 rounded-full" onClick={() => setIsPlaying(!isPlaying)}>
+          <button className="bg-white p-2 rounded-full" onClick={handleClick}>
             {isPlaying ? <Pause /> : <Play />}
           </button>
         </div>
@@ -36,6 +53,7 @@ export function Player() {
         <Volume />
       </div>
 
+      <audio ref={audioRef} />
     </div>
   ) 
 }
