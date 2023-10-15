94cdb68a5fa2c93fb43e396dd7e9cd8f720a5151
xavigu <xavigu91@gmail.com>
2023-10-15 19:08:05 +0200

    Create api function to get playlist and songs from the id

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -9,12 +9,23 @@ export function CardPlayButton({ id }) {
     setCurrentMusic 
   } = usePlayerStore(state => state);
 
+  const isPlayingPlaylist = isPlaying && currentMusic.playlist.id === id;
+
   const handleClick = () => {
-    setCurrentMusic({ playlist: { id } });
-    setIsPlaying(!isPlaying);
+    if (isPlayingPlaylist) {
+      setIsPlaying(false);
+      return
+    }
+
+    fetch(`/api/get-info-playlist.json?id=${id}`)
+      .then(res => res.json())
+      .then(data => {
+        const { songs, playlist } = data;
+        setIsPlaying(true);
+        setCurrentMusic({ playlist, song: songs[0], songs });
+      }) 
   }
 
-  const isPlayingPlaylist = isPlaying && currentMusic.playlist.id === id;
 
   return (
     <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
