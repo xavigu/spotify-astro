ca7038bd2ebce9651ea7afb597afc4540000cbd8
xavigu <xavigu91@gmail.com>
2023-10-22 19:07:29 +0200

    Add songsTable and cardPlayButton components into [id] page

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,7 +1,7 @@
 import { Play, Pause } from "./Player";
 import { usePlayerStore } from "../store/playerStore";
 
-export function CardPlayButton({ id }) {
+export function CardPlayButton({ id, size='small' }) {
   const { 
     currentMusic, 
     isPlaying, 
@@ -24,12 +24,15 @@ export function CardPlayButton({ id }) {
         setIsPlaying(true);
         setCurrentMusic({ playlist, song: songs[0], songs });
       }) 
-  }
+  };
+
+  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5';
 
 
   return (
-    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
-      {isPlayingPlaylist ? <Pause/> : <Play/>}
+    <button onClick={handleClick} className="card-play-button rounded-full
+                                           bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400">
+      {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
     </button>
   );
 }
